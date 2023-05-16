c983e346061992357408e4d16cf73d4c6d97309b
klimklimson <ki.klimashenko@gmail.com>
2023-05-16 23:26:34 +0200

    toCamelCase function

---
+++ CodeWars5.js
@@ -0,0 +1,18 @@
+function toCamelCase(str) {
+    const regExp = /\*|-|_|\$/g;
+    let res="";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '_' || str[i] === '-') {
+            res += str.charAt(i + 1).toUpperCase();
+            i++;
+        }
+        else
+            {
+                res += str[i];
+            }
+    }
+    res = res.replace(regExp, '');
+    return res;
+}
+
+console.log(toCamelCase("Man_of-culture"));
